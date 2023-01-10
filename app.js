874d21008c18d9ed4919369e8c3999dbef4c3939
fehleques <fehleques@gmail.com>
2023-01-10 20:29:12 -0300

    Fixed CSS transitions

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-const sections = document.querySelectorAll(".main-container");
+// const sections = document.querySelectorAll(".main-container");
+
+// const options = {
+//     threshold: 0.6,
+//     rootMargin: "",
+// };
+
+// const observer = new IntersectionObserver(function (entries, observer) {
+
+//     entries.forEach(entry => {
+
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add("is--active");
+
+//         } else {
+            
+//             entry.target.classList.remove("is--active");
+//         };
+
+//     });
+// }, options);
+
+// sections.forEach(section => {
+//     observer.observe(section);
+// });
+
+const target = document.querySelectorAll(".is--animated");
 
 const options = {
-    threshold: 0.6,
-    rootMargin: "",
+    threshold: 0.95,
+    rootMargin: "500px 0px 500px 0px",
 };
 
 const observer = new IntersectionObserver(function (entries, observer) {
@@ -20,6 +46,6 @@ const observer = new IntersectionObserver(function (entries, observer) {
     });
 }, options);
 
-sections.forEach(section => {
-    observer.observe(section);
+target.forEach(entries => {
+    observer.observe(entries);
 });
