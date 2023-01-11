b9afdedd230f8c76e233e4c549915cc8feb74f52
fehleques <fehleques@gmail.com>
2023-01-10 21:17:46 -0300

    CSS updates

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-// const sections = document.querySelectorAll(".main-container");
-
-// const options = {
-//     threshold: 0.6,
-//     rootMargin: "",
-// };
-
-// const observer = new IntersectionObserver(function (entries, observer) {
-
-//     entries.forEach(entry => {
-
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("is--active");
-
-//         } else {
-            
-//             entry.target.classList.remove("is--active");
-//         };
-
-//     });
-// }, options);
-
-// sections.forEach(section => {
-//     observer.observe(section);
-// });
-
 const target = document.querySelectorAll(".is--animated");
 
 const options = {
